ebc2a120c4a2daa0497348309ed75a3025b498fe
Sekarre <zimny2121@gmail.com>
2022-07-17 20:03:06 +0200

    added security - todo: fix ws authentication

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AlertService} from "../services/alert.service";
+import {AuthService} from "../services/auth.service";
+import {COMMON_ERROR_MESSAGE} from "../domain/CommonMessages";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginFormGroup!: FormGroup;
+  fieldTextType!: boolean;
+
+  constructor(private authService: AuthService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              private alertService: AlertService) {
+  }
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.formBuilder.group({
+      user: this.formBuilder.group({
+        login: new FormControl('',
+          [Validators.required, Validators.minLength(2)]),
+        password: new FormControl('',
+          [Validators.required, Validators.minLength(2)])
+      })
+    });
+  }
+
+  login() {
+    this.authService.login(this.loginFormGroup.get('user')!.get('login')!.value, this.loginFormGroup.get('user')!.get('password')!.value)
+      .subscribe(
+        response => {
+          this.authService.setToken(response.token);
+          this.router.navigateByUrl('/chat');
+        },
+        error => {
+          this.alertService.error(COMMON_ERROR_MESSAGE);
+        }
+      );
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserCredentials} from '../domain/UserCredentials';
+import {Observable} from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  authUrl = 'http://localhost:8080/api/v1/auth/login';
+
+  constructor(private http: HttpClient) {
+  }
+
+  login(login: string, password: string): Observable<any> {
+
+    const userCredentials = new UserCredentials();
+    userCredentials.username = login;
+    userCredentials.password = password;
+
+    return this.http.post<TokenResponse>(`${this.authUrl}`, userCredentials);
+  }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('key');
+  }
+
+  getToken(): string {
+    return sessionStorage.getItem('token')!;
+  }
+
+  setToken(token: string) {
+    sessionStorage.setItem('token', token);
+  }
+}
+
+interface TokenResponse {
+  token: string
+}
+
