1a01abd8adedddc5b112af27958e26b25be597d0
Sekarre <zimny2121@gmail.com>
2022-08-07 21:53:20 +0200

    Fix on disconnecting from SSE on component destroy

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -12,6 +12,7 @@ import {SseEventMessage} from "../domain/SseEventMessage";
 export class NotificationComponent implements OnInit {
 
   private serverUrl: string = environment.baseUrl + ApiPaths.SSE;
+  private source: EventSource | undefined;
   public events: SseEventMessage[] = [];
 
   constructor(private authService: AuthService) { }
@@ -20,17 +21,27 @@ export class NotificationComponent implements OnInit {
     this.connect();
   }
 
+  ngOnDestroy(): void {
+    this.disconnect();
+  }
+
   connect(): void {
-    let source = new EventSource(this.serverUrl + "?token=Bearer " + this.authService.getToken());
+    this.source = new EventSource(this.serverUrl + "?token=Bearer " + this.authService.getToken());
     console.log('Connecting to eventSource');
-    source.addEventListener('message', message => {
+    this.source.addEventListener('message', message => {
       let eventMessage = this.parseEventJson(message);
       console.log(eventMessage);
       this.events.push(eventMessage)
     });
   }
 
-  private parseEventJson(message: MessageEvent): SseEventMessage {
+  disconnect() {
+    console.log('SSe closed');
+    this.source?.close();
+  }
+
+  parseEventJson(message: MessageEvent): SseEventMessage {
+    console.log('parsing json');
     let parsedJson = JSON.parse(message.data);
     return new SseEventMessage(parsedJson[0].data, parsedJson[1].data);
   }
